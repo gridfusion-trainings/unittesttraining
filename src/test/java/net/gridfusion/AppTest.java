2e53ff7e0f9ec73b9ad31768d1281d7b92aedbde
gridfusion <michael.palotas@gridfusion.net>
2015-08-14 07:07:55 +0200

    initial commit

---
+++ src/test/java/net/gridfusion/AppTest.java
@@ -0,0 +1,20 @@
+package net.gridfusion;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void firsttest1() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void firsttest2() {
+        Assert.assertTrue(true);
+    }
+}
---
+++ src/test/java/net/gridfusion/ExpectedExceptionsTest.java
@@ -0,0 +1,14 @@
+package net.gridfusion;
+
+import org.junit.Test;
+
+/**
+ * Created by gridfusion on 13/08/15.
+ */
+public class ExpectedExceptionsTest {
+
+    @Test(expected=RuntimeException.class)
+    public void expectedException() throws Exception {
+        throw new RuntimeException();
+    }
+}
